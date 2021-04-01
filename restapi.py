85508dbf0c26af3d4ccb967888d4927cbc10603c
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-01 13:28:34 +0530

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -3,13 +3,15 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
-client = MongoClient("mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+# establishing connection to mongodb
+client = MongoClient(
+    "mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 
 
 class MongoAPIUser:
     def __init__(self, data):
-        self.client = MongoClient("mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+        self.client = MongoClient(
+            "mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
         cursor = self.client['insent']
         self.collection = cursor['Users']
         self.data = data
@@ -17,6 +19,11 @@ class MongoAPIUser:
     def read_user(self):
         documents = self.collection.find()
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+
+    def read_user_find_one(self, data):
+        documents = self.collection.find_one(data, {"_id": 0})
+        output = jsonify(documents)
         return output
 
     def write_user(self, data):
@@ -32,9 +39,23 @@ class MongoAPIUser:
         output = {'Status': 'Successfully Updated' if response.modified_count > 0 else "Nothing was updated."}
         return output
 
+    def delete_user(self, data):
+        filt = data['Filter']
+        response = self.collection.delete_one(filt)
+        output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
+        return output
+
+    def aggregate_find_user(self, data):
+        filt = self.data['Filter']
+        response = self.collection.aggregate([{"$match": filt}])
+        output = [{item: data[item] for item in data if item != '_id'} for data in response]
+        return output
+
+
 class MongoAPIVisitor:
     def __init__(self, data):
-        self.client = MongoClient("mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+        self.client = MongoClient(
+            "mongodb+srv://sameena:sameena@cluster0.nkw8r.mongodb.net/insent?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
         cursor = self.client['insent']
         self.collection = cursor['Visitors']
         self.data = data
@@ -44,44 +65,80 @@ class MongoAPIVisitor:
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
         return output
 
+    def read_visitor_find_one(self, data):
+        documents = self.collection.find_one(data, {"_id": 0})
+        output = jsonify(documents)
+        return output
+
     def write_visitor(self, data):
         response = self.collection.insert_one(data)
         output = {'Status': 'Successfully Inserted',
                   'Document_ID': str(response.inserted_id)}
         return output
-    
+
     def update_visitor(self, data):
         filt = self.data['Filter']
         updated_data = {"$set": self.data['DataToBeUpdated']}
         response = self.collection.update_one(filt, updated_data)
         output = {'Status': 'Successfully Updated' if response.modified_count > 0 else "Nothing was updated."}
         return output
-    
+
+    def delete_visitor(self, data):
+        filt = data['Filter']
+        response = self.collection.delete_one(filt)
+        output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
+        return output
+
+    def aggregate_find_visitor(self, data):
+        filt = self.data['Filter']
+        response = self.collection.aggregate([{"$match": filt}])
+        output = [{item: data[item] for item in data if item != '_id'} for data in response]
+        return output
+
+
 @app.route('/read_user', methods=['GET'])
 def mongo_read_user():
     data = request.json
-    if data is None or data == {}:
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
     obj1 = MongoAPIUser(data)
     response = obj1.read_user()
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
 
+
 @app.route('/read_visitor', methods=['GET'])
 def mongo_read_visitor():
+    data = request.json
+    obj1 = MongoAPIVisitor(data)
+    response = obj1.read_visitor()
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/read_user_find_one', methods=['GET'])
+def mongo_read_one_user():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPIUser(data)
+    response = obj1.read_user_find_one(data)
+    return response
+
+
+@app.route('/read_visitor_find_one', methods=['GET'])
+def mongo_read_one_visitor():
     data = request.json
     if data is None or data == {}:
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPIVisitor(data)
-    response = obj1.read_visitor()
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
+    response = obj1.read_visitor_find_one(data)
+    return response
+
 
 @app.route('/insert_user', methods=['POST'])
 def mongo_write_user():
@@ -92,6 +149,7 @@ def mongo_write_user():
                     status=200,
                     mimetype='application/json')
 
+
 @app.route('/insert_visitor', methods=['POST'])
 def mongo_write_visitor():
     data = request.json
@@ -101,6 +159,7 @@ def mongo_write_visitor():
                     status=200,
                     mimetype='application/json')
 
+
 @app.route('/update_user', methods=['PUT'])
 def mongo_update_user():
     data = request.json
@@ -109,6 +168,7 @@ def mongo_update_user():
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
+
 
 @app.route('/update_visitor', methods=['PUT'])
 def mongo_update_visitor():
@@ -120,5 +180,60 @@ def mongo_update_visitor():
                     mimetype='application/json')
 
 
+@app.route('/delete_user', methods=['DELETE'])
+def mongo_delete_user():
+    data = request.json
+    if data is None or data == {} or 'Filter' not in data:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPIUser(data)
+    response = obj1.delete_user(data)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/delete_visitor', methods=['DELETE'])
+def mongo_delete_visitor():
+    data = request.json
+    if data is None or data == {} or 'Filter' not in data:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPIVisitor(data)
+    response = obj1.delete_visitor(data)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/read_user_find_aggregate', methods=['GET'])
+def mongo_read_user_aggregate():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPIUser(data)
+    response = obj1.aggregate_find_user(data)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/read_visitor_find_aggregate', methods=['GET'])
+def mongo_read_visitor_aggregate():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPIVisitor(data)
+    response = obj1.aggregate_find_visitor(data)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=5002, host='0.0.0.0')
